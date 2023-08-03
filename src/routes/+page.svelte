4ec5c74161ece29f3886d14959ef40c9ba762ae0
reaperhound <reaperhound69004@gmail.com>
2023-08-03 13:06:55 +0530

    Initail commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div>
+  <h1>Welcome to SvelteKit</h1>
+  <a href="/ss">SS</a>
+</div>
+
+<style>
+    div{
+        height: 100vh;
+        width: 100%;
+        position: absolute;
+        color: aliceblue;
+        background-color: black;
+      }
+      a{
+        color: aqua;
+      }
+</style>
---
+++ src/routes/ss/+page.svelte
@@ -0,0 +1,16 @@
+<div>
+  <h2>ldkkncklkl</h2>
+    <h1>Welcome to SS</h1>
+  </div>
+  
+  <style>
+      div{
+        position: absolute;
+        margin: 0;
+        width: 100%;
+        height: 100vh;
+        color: aliceblue;
+        background-color: black;
+      }
+  </style>
+  
