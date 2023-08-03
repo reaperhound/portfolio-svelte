4f2fde04a25f0846ebae4103abe0d73fd11f13dd
reaperhound <reaperhound69004@gmail.com>
2023-08-03 14:54:40 +0530

    Initial edits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,36 @@
-<div>
-  <h1>Welcome to SvelteKit</h1>
-  <a href="/ss">SS</a>
-</div>
+<main>
+  <div class="hero">
+    <h1>Ajmal Khan</h1>
+    <h3>Web Developer</h3>
+    <a href="/about">About</a>
+  </div>
+</main>
 
 <style>
-    div{
-        height: 100vh;
-        width: 100%;
-        position: absolute;
-        color: aliceblue;
-        background-color: black;
-      }
-      a{
-        color: aqua;
-      }
+  main {
+    font-family: "Poppins";
+    position: absolute;
+    height: 100vh;
+    width: 100%;
+    background-color: #272929;
+  }
+  .hero{
+    margin-left: 158px;
+    margin-top: 300px;
+    text-align: center;
+    width: fit-content;
+  }
+  h1 {
+    color: #f0ba63;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 400;
+    margin-bottom: 15px;
+  }
+  h3 {
+    color: #209ac8;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+  }
 </style>
